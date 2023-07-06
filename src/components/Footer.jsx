529d62e11c62dee38ed012776888acb2cc6d76c8
Nishant4coding <srivastava4nishant@gmail.com>
2023-07-06 17:58:16 +0530

    css update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <HStack p={3} bg="rgb(96, 255, 167)" justify="center" position='fixed' w='100%' bottom={0}>
+    <HStack p={3} bg="green.900" justify="center" position='fixed' w='100%' bottom={0}>
       <Text color="white">
         Designed and developed by{" "}
         <Link href="https://github.com/Nishant4coding" isExternal color="cyan.200">
