0663f42f3876484fa2fcdc8803d0b4667997300a
Nishant4coding <srivastava4nishant@gmail.com>
2023-08-08 17:58:37 +0530

    update

--- src/App.js
+++ src/App.js
@@ -3,12 +3,18 @@ import Footer from "./components/Footer";
 import LandingPage from "./components/LandingPage";
 import ResumeForm from "./components/ResumeForm";
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter , Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
-  
+  <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/show" element={<ResumeForm />} />
+      </Routes>
+    </BrowserRouter>
+    <Footer/>
       
     </div>
   );
